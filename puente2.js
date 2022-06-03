dab54361909e895197a0a018509ade997b441221
Juan31452 <jcs1011@gmail.com>
2022-06-03 00:12:21 -0500

    se modifica funcion obtenerfecha

--- puente2.js
+++ puente2.js
@@ -1,15 +1,44 @@
 function obtenerfecha()
 {   
-    
-    var mes = document.getElementById("mes").value;
-    var ano = document.getElementById("ano").value;
+  //var mes = document.getElementById("mes").value;
+  //var ano = document.getElementById("ano").value;
+  
+ //   var datosfecha=
+ //   {
+ //     "mes": mes,
+ //     "ano": ano
+ //   };
+ //   console.log(datosfecha); 
+
+ // Esta es la variable que vamos a pasar
+
+		var mes=$("#mes").val();
+    var ano=$("#ano").val();
+
+ 
     console.log(mes+" "+ano); 
 
-    //$.ajax({
-     //   data : datosfecha,
-      //  url : "../php/consultaventas.php",
-       // type : 'GET'
-   // })
+		// Enviamos la variable de javascript a archivo.php
+
+		$.post("../php/consultaventas1.php",{"mes":mes,"ano":ano},function(respuesta){
+
+			alert(respuesta);
+    
+      let cad = ' ';
+      for (let articulo of respuesta) 
+      {
+        cad += `<tr>
+        <td>${articulo.Fecha}</td>
+        <td>${articulo.Producto}</td>
+        <td>${articulo.Cantidad}</td>
+        <td>${articulo.Valor_Unitario}</td>
+        </tr>`;
+      }
+      document.getElementById("datostabla").innerHTML = cad; 
+      
+    });
+  
+    
 }
 
 function listarArticulos() 
